420a9ec00350b3311641af0b2675da5f2b429cf4
raphlev <raphleveque@hotmail.fr>
2021-02-20 11:18:34 +0100

    u

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -2,16 +2,6 @@ import { FETCH_CART, ADD_TO_CART, UPDATE_CART_QTY, REMOVE_FROM_CART, EMPTY_CART,
 
 export default (cart = {}, action) => {
   switch (action.type) {
-    // case LIKE:
-    //   return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-    // case CREATE:
-    //   return [...posts, action.payload];
-    // case UPDATE:
-    //   return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-    // case DELETE:
-    //   return posts.filter((post) => post._id !== action.payload);
-    // default:
-    //   return posts;
     case FETCH_CART:
       return action.payload;
     case ADD_TO_CART:
